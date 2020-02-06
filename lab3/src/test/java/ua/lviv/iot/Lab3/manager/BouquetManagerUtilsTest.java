139182066dd6264862ec063c48743bc21d264023
Daniil Oleshchuk <danieloleshuk@gmail.com>
2020-02-06 17:48:02 +0200

    reformated code

--- lab3/src/test/java/ua/lviv/iot/Lab3/manager/BouquetManagerUtilsTest.java
+++ lab3/src/test/java/ua/lviv/iot/Lab3/manager/BouquetManagerUtilsTest.java
@@ -1,4 +1,3 @@
-
 package ua.lviv.iot.Lab3.manager;
 
 import org.junit.jupiter.api.Assertions;
@@ -13,11 +12,13 @@ public class BouquetManagerUtilsTest extends BaseBouquetManagerTest {
         Assertions.assertEquals(40, bouquet.get(1).getPriceInUAH());
         Assertions.assertEquals(70, bouquet.get(2).getPriceInUAH());
     }
-    @Test void testSortFlowersInBouquetByHeight(){
-        BouquetManagerUtils.sortFlowersInBouquetByHeight(bouquet,SortType.ASCENDING);
-        Assertions.assertEquals(10,bouquet.get(0).getHeightInSm());
-        Assertions.assertEquals(15,bouquet.get(1).getHeightInSm());
-        Assertions.assertEquals(20,bouquet.get(2).getHeightInSm());
+
+    @Test
+    void testSortFlowersInBouquetByHeight() {
+        BouquetManagerUtils.sortFlowersInBouquetByHeight(bouquet, SortType.ASCENDING);
+        Assertions.assertEquals(10, bouquet.get(0).getHeightInSm());
+        Assertions.assertEquals(15, bouquet.get(1).getHeightInSm());
+        Assertions.assertEquals(20, bouquet.get(2).getHeightInSm());
     }
 
 }
