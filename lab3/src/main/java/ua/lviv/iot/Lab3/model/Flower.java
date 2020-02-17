79bbf7f40d1d7678a8d714bd4acccaf9c32b76cc
Daniil Oleshchuk <danieloleshuk@gmail.com>
2020-02-17 17:31:49 +0200

    added additional int field

--- lab3/src/main/java/ua/lviv/iot/Lab3/model/Flower.java
+++ lab3/src/main/java/ua/lviv/iot/Lab3/model/Flower.java
@@ -2,14 +2,16 @@ package ua.lviv.iot.Lab3.model;
 
 public class Flower extends Product {
     private String typeOfFlower;
+    private int howMuchDaysCanLive;
 
     Flower() {
-        this(null, null, 0, 0, null);
+        this(null, null, 0, 0, null,0);
     }
 
-    public Flower(String typeOfProduct, String color, int heightInSm, double priceInUAH, String typeOfFlower) {
+    public Flower(String typeOfProduct, String color, int heightInSm, double priceInUAH, String typeOfFlower, int howMuchDaysCanLive) {
         super(typeOfProduct, color, heightInSm, priceInUAH);
         this.typeOfFlower = typeOfFlower;
+        this.howMuchDaysCanLive = howMuchDaysCanLive;
     }
 
     public String getTypeOfFlower() {
@@ -18,5 +20,13 @@ public class Flower extends Product {
 
     public void setTypeOfFlower(String typeOfFlower) {
         this.typeOfFlower = typeOfFlower;
+    }
+
+    public int getHowMuchDaysCanLive() {
+        return howMuchDaysCanLive;
+    }
+
+    public void setHowMuchDaysCanLive(int howMuchDaysCanLive) {
+        this.howMuchDaysCanLive = howMuchDaysCanLive;
     }
 }
